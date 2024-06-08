2677693fea653be962879ff3dba928ae57774897
2023JoshuaP <jphilco@unsa.edu.pe>
2024-06-08 16:21:21 -0500

    Definicion de la clase BufferManager, intermediario entre las solicitudes de BufferPool con el Disco.

---
+++ BufferManager.h
@@ -0,0 +1,28 @@
+#ifndef BUFFERMANAGER_H
+#define BUFFERMANAGER_H
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include "./BufferPool.cpp"
+#include "./PageTable.cpp"
+using namespace std;
+
+class BufferManager {
+    public:
+        BufferPool bufferPool;
+        PageTable pageTable;
+    public:
+        BufferManager();
+        ~BufferManager();
+        void crearBufferPoolSegunNumFrames(int numFrames);
+        void establecerLimiteDeFrames(int pesoBytesBLoque);
+        void obtenerUnaPagina(int numPagina);
+        void mostrarUnaPagina(int numPagina);
+        void mostrarPageTAble();
+        void dejarDeUsarUnaPagina(int numPagina);
+};
+
+#endif
